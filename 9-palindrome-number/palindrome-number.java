18f4fe06cb75f3ff3af43adc13caed738fae81bb
Sivaruhith <230701320@rajalakshmi.edu.in>
2026-02-05 21:26:37 +0530

    Time: 5 ms (74.52%) | Memory: 46.1 MB (48.35%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x%10 == 0 && x != 0) ){
+            return false;
+        }
+        int rev=0;
+        int org=x;
+        while(x != 0){
+            int sum=x%10;
+            rev = (rev*10) + sum; 
+            x=x/10;
+        }
+        return org == rev;
+    }
+}
