9fb2a6e00ebfdac6c17e3489d61e0f90106ee1f9
Sivaruhith <230701320@rajalakshmi.edu.in>
2026-04-03 17:15:26 +0530

    Time: 103 ms (57.64%) | Memory: 51.2 MB (45.53%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+    //     if(n<=1){
+    //         return 0;
+    //     }
+    //     int count=0;
+    //     for(int i=2;i<n;i++){
+    //         if(isPrime(i)){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    // public boolean isPrime(int n){
+    //     for(int i=2;i*i<=n;i++){
+    //         if(n%i==0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+
+        // Assume all numbers are prime initially
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Sieve logic
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Count primes
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+
+        return count;
+    }
+    
+}
