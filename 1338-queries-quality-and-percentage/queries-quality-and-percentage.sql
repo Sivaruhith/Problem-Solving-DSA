adc6437afc5534152713476b343d16b2f239b694
Sivaruhith <230701320@rajalakshmi.edu.in>
2026-09-19 14:28:37 +0530

    Time: 423 ms (38.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,4 @@
+SELECT query_name,ROUND(AVG(rating/position),2) as quality,
+ROUND(COUNT(CASE WHEN rating<3 THEN 1 END)* 100.0 / COUNT(rating),2) as poor_query_percentage
+FROM Queries
+GROUP BY query_name;
